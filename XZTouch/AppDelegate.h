721e37b3f799ec80764ba9550c137f99c7762495
kkxz79 <kkxz79@126.com>
2018-10-22 11:09:07 +0800

    Initial Commit

---
+++ XZTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZTouch
+//
+//  Created by kkxz on 2018/10/22.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZTouch/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XZTouch
+//
+//  Created by kkxz on 2018/10/22.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
